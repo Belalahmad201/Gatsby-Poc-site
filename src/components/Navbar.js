a9ad2be0800a21e13b9162eda4d000a0a4f606b5
Belalahmad201 <ahmadbelal802211@gmail.com>
2025-09-11 00:45:45 +0530

    Initial Gatsby POC complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Navbar = () => (
+  <nav style={{ marginBottom: "2rem" }}>
+    <Link to="/">Home</Link> |{" "}
+    <Link to="/about">About</Link> |{" "}
+    <Link to="/contact">Contact</Link>
+  </nav>
+)
+
+export default Navbar
+
